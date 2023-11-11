aa35565be989ed724eaf5fa2eb5aa0651f76cdc4
Timur Magomedov <mew678hh@gmail.com>
2023-11-11 18:57:56 +0300

    Change README

---
+++ scheta104/scheta104.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+struct bank_account {
+    int account_number;
+    string owners_name;
+    int balance;
+    int new_balance;
+};
+void order(bank_account& p) {
+    cout << "Введите номер счёта: ";
+    cin >> p.account_number;
+    cout << "Введите имя владельца: ";
+    cin >> p.owners_name;
+    cout << "Введите баланс: ";
+    cin >> p.balance;
+    cout << "Введите новый баланс: ";
+    cin >> p.new_balance;
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    bank_account card;
+    order(card);
+    cout << "Ваш счёт: " << card.owners_name << " " << card.account_number << " " << card.new_balance;
+    return 0;
+}
